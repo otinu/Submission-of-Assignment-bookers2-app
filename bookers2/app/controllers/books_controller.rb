128b406f7de236e6fd4d3ffc918527075addfe46
otinu <fitshter@gmail.com>
2021-03-30 14:47:17 +0000

    save

--- bookers2/app/controllers/books_controller.rb
+++ bookers2/app/controllers/books_controller.rb
@@ -3,15 +3,15 @@ class BooksController < ApplicationController
   def index
       @book = Book.new
      @books = Book.all
-     
+
 
   end
 
   def show
-     book = Book.joins("LEFT OUTER JOIN users ON books.user_id = users.id ")
+     #book = Book.joins("LEFT OUTER JOIN users ON books.user_id = users.id ")
     @book = Book.find(params[:id])
     #@user = User.find_by(id: params[:Book.user_id])
-    
+
     @user = User.find(params[:user_id])
   end
 
