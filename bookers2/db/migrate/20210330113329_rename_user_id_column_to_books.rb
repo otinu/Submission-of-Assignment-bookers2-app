05f4313f7581074f234c60464b9fed246057856c
otinu <fitshter@gmail.com>
2021-04-05 06:12:10 +0000

    after-feedback_remove-extra-migrete

--- bookers2/db/migrate/20210330113329_rename_user_id_column_to_books.rb
+++
@@ -1,6 +0,0 @@
-class RenameUserIdColumnToBooks < ActiveRecord::Migration[5.2]
-  def change
-    rename_column :books, :user_id, :integer
-    add_reference :books, :user, foreign_key: true
-  end
-end
--- bookers2/db/migrate/20210330142141_remove_foreign_key.rb
+++
@@ -1,5 +0,0 @@
-class RemoveForeignKey < ActiveRecord::Migration[5.2]
-  def change
-     remove_foreign_key :books, :user_id
-  end
-end
