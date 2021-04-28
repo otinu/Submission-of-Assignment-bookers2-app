b0402c18ea554ba3e34011f43373d243d851bde0
otinu <fitshter@gmail.com>
2021-04-28 09:46:30 +0000

    save

--- bookers2.clone/db/seeds.rb
+++ bookers2.clone/db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+5.times do |n|
+  name = Faker::Name.name[0...20]
+  email    = "example-#{n+1}@test.com"
+  password = "password"
+  User.create!(name: name, email: email, password: password)
+end
+
+Book.create!(title: "Test",body: "first",user_id: 1)
+
+5.times do |n|
+  title = Faker::Book.title
+  body  = "テスト"
+  Book.create!(title: title, body: body, user_id: 1)
+end
