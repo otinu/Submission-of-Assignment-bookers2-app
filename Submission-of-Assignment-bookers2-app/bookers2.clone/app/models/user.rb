37fc8c163c19dff7c95b13db695c1873fe7c6ab3
otinu <fitshter@gmail.com>
2021-04-30 08:47:53 +0000

    save

--- Submission-of-Assignment-bookers2-app/bookers2.clone/app/models/user.rb
+++ Submission-of-Assignment-bookers2-app/bookers2.clone/app/models/user.rb
@@ -15,11 +15,28 @@ class User < ApplicationRecord
   has_many :books, dependent: :destroy
   has_many :favorites, dependent: :destroy
 
+
   has_many :active_relationships, class_name:  "Relationship", foreign_key: "follower_id", dependent:  :destroy
   has_many :follows, through: :active_relationships, source: :followed
-  
+
   has_many :passive_relationships, class_name:  "Relationship", foreign_key: "followed_id", dependent:   :destroy
   has_many :followers, through: :passive_relationships, source: :follower
+
+  # ユーザーをフォローする
+  def follow(other_user)
+    follows << other_user
+  end
+
+  # ユーザーをフォロー解除する
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  # 現在のユーザーがフォローしてたらtrueを返す
+  def follows?(other_user)
+    follows.include?(other_user)
+  end
+
 
 
   # ここまで手動で追記=============================================
