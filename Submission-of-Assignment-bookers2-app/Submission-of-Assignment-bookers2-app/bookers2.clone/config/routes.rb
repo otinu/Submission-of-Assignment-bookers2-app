99b57b3bd0bab317c54e0c2dbc647f0273b3de31
otinu <fitshter@gmail.com>
2021-06-13 00:06:22 +0000

    [test]for_portfolio_post/index

---
+++ Submission-of-Assignment-bookers2-app/Submission-of-Assignment-bookers2-app/bookers2.clone/config/routes.rb
@@ -0,0 +1,25 @@
+Rails.application.routes.draw do
+  # ここから手動で追記=============================================
+  root 'homes#top'
+  get 'home/about' => 'homes#about', as: 'about'
+  get 'home/test' => 'homes#test', as: 'test'
+  devise_for :users
+
+  resources :books, only: [:index, :show, :create, :update, :edit, :destroy] do
+    resources :post_comments, only: [:create, :destroy]
+    resource :favorites, only: [:create, :destroy]
+  end
+
+  resources :users, only: [:index, :show, :edit, :update]
+  patch 'users_path' => 'users#show'
+
+  resources :users do
+    member do
+      get :follows, :followers
+    end
+  end
+
+  resources :relationships, only: [:create, :destroy]
+
+  # ここまで手動で追記=============================================
+end
